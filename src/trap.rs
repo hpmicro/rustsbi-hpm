1bade2407c5566132af8a1b80c93450357b71c73
Zhouqi Jiang <luojia@hust.edu.cn>
2024-07-15 11:59:02 +0800

    trap: change to direct mode, as HPM only supports direct mode
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- src/trap.rs
+++ src/trap.rs
@@ -1,36 +1,36 @@
-use core::arch::asm;
-use fast_trap::trap_entry;
+// use core::arch::asm;
+// use fast_trap::trap_entry;
 
-// 中断向量表
-//
-// # Safety
-//
-// 裸函数。
-#[naked]
-pub(crate) unsafe extern "C" fn trap_handler() {
-    asm!(
-        ".align 2",
-        ".option push",
-        ".option norvc",
-        "j {default}", // exception
-        "j {default}", // supervisor software
-        "j {default}", // reserved
-        "j {msoft} ",  // machine    software
-        "j {default}", // reserved
-        "j {default}", // supervisor timer
-        "j {default}", // reserved
-        "j {mtimer}",  // machine    timer
-        "j {default}", // reserved
-        "j {default}", // supervisor external
-        "j {default}", // reserved
-        "j {default}", // machine    external
-        ".option pop",
-        default = sym trap_entry,
-        mtimer  = sym trap_entry,
-        msoft   = sym trap_entry,
-        options(noreturn)
-    )
-}
+// // 中断向量表
+// //
+// // # Safety
+// //
+// // 裸函数。
+// #[naked]
+// pub(crate) unsafe extern "C" fn trap_handler() {
+//     asm!(
+//         ".align 2",
+//         ".option push",
+//         ".option norvc",
+//         "j {default}", // exception
+//         "j {default}", // supervisor software
+//         "j {default}", // reserved
+//         "j {msoft} ",  // machine    software
+//         "j {default}", // reserved
+//         "j {default}", // supervisor timer
+//         "j {default}", // reserved
+//         "j {mtimer}",  // machine    timer
+//         "j {default}", // reserved
+//         "j {default}", // supervisor external
+//         "j {default}", // reserved
+//         "j {default}", // machine    external
+//         ".option pop",
+//         default = sym trap_entry,
+//         mtimer  = sym trap_entry,
+//         msoft   = sym trap_entry,
+//         options(noreturn)
+//     )
+// }
 
 // machine timer 中断代理
 //
