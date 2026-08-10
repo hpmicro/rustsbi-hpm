ddba61325f8e484d518a0050bfa61924e487582a
tfx2001 <tfx2001@outlook.com>
2026-08-10 23:49:34 +0800

    feat(board): migrate femc init to new hpm-hal API
    hpm_hal::femc::Sdram::new_16bit_cs0 lacks support for 13-bit address.
    Introduce a new API hpm_hal::femc::Sdram::new_cs0 to support SDRAM
    variants ranging from 11-bit to 13-bit addresses.
    
    Signed-off-by: tfx2001 <tfx2001@outlook.com>

--- src/board/hpm6360evk/sdram.rs
+++ src/board/hpm6360evk/sdram.rs
@@ -1,4 +1,7 @@
-use hpm_hal::femc::{Bank2Sel, CasLatency, ColAddrBits, MemorySize, SdramChip, SdramPortSize};
+use hpm_hal::femc::{
+    CasLatency, ColAddrBits, MemorySize, SdramChip,
+    geometry::{Address13, Banks4, Data16},
+};
 
 #[cfg(feature = "sdram-rw-test")]
 pub const SIZE: usize = 32 * 1024 * 1024;
@@ -10,20 +13,18 @@ pub struct SdramInfo {
 pub struct Hpm6360evkSdram;
 
 impl SdramChip for Hpm6360evkSdram {
+    type AddressWidth = Address13;
+    type DataWidth = Data16;
+    type BankCount = Banks4;
+
     fn col_addr_bits(&self) -> ColAddrBits {
         ColAddrBits::_9BIT
     }
     fn cas_latency(&self) -> CasLatency {
         CasLatency::_3
     }
-    fn bank_num(&self) -> Bank2Sel {
-        Bank2Sel::BANK_NUM_4
-    }
     fn size(&self) -> MemorySize {
         MemorySize::_32MB
-    }
-    fn port_size(&self) -> SdramPortSize {
-        SdramPortSize::_16BIT
     }
     fn refresh_count(&self) -> u32 {
         8192
@@ -56,53 +57,29 @@ impl SdramChip for Hpm6360evkSdram {
 
 macro_rules! init {
     ($p:ident, $delay:ident) => {{
-        let sdram = hpm_hal::femc::Sdram::new_16bit_cs0_a12(
+        let sdram = hpm_hal::femc::Sdram::new_cs0(
             $p.FEMC,
-            // Address pins A0-A12.
-            $p.PB18,
-            $p.PB19,
-            $p.PB20,
-            $p.PB21,
-            $p.PB31,
-            $p.PB30,
-            $p.PB29,
-            $p.PB28,
-            $p.PB27,
-            $p.PB26,
-            $p.PB17,
-            $p.PB25,
-            $p.PB24,
-            // Bank address BA0-BA1.
-            $p.PB15,
-            $p.PB16,
-            // Data pins DQ0-DQ15.
-            $p.PB00,
-            $p.PA31,
-            $p.PA30,
-            $p.PA29,
-            $p.PA28,
-            $p.PA27,
-            $p.PA26,
-            $p.PA25,
-            $p.PB02,
-            $p.PB03,
-            $p.PB04,
-            $p.PB05,
-            $p.PB06,
-            $p.PB07,
-            $p.PB08,
-            $p.PB09,
-            // Data mask DM0-DM1.
-            $p.PB01,
-            $p.PB10,
-            // Control: DQS, CLK, CKE, RAS, CAS, WE, CS0.
-            $p.PX07,
-            $p.PB22,
-            $p.PB23,
-            $p.PB13,
-            $p.PB12,
-            $p.PB11,
-            $p.PB14,
+            hpm_hal::femc::SdramPins {
+                address: (
+                    $p.PB18, $p.PB19, $p.PB20, $p.PB21, $p.PB31, $p.PB30, $p.PB29, $p.PB28,
+                    $p.PB27, $p.PB26, $p.PB17, $p.PB25, $p.PB24,
+                ),
+                banks: ($p.PB15, $p.PB16),
+                data: (
+                    $p.PB00, $p.PA31, $p.PA30, $p.PA29, $p.PA28, $p.PA27, $p.PA26, $p.PA25,
+                    $p.PB02, $p.PB03, $p.PB04, $p.PB05, $p.PB06, $p.PB07, $p.PB08, $p.PB09,
+                ),
+                masks: ($p.PB01, $p.PB10),
+                control: hpm_hal::femc::ControlPins {
+                    dqs: $p.PX07,
+                    clk: $p.PB22,
+                    cke: $p.PB23,
+                    ras: $p.PB13,
+                    cas: $p.PB12,
+                    we: $p.PB11,
+                    cs: $p.PB14,
+                },
+            },
             $crate::board::board_impl::sdram::Hpm6360evkSdram,
         );
 
--- src/board/hpm6750evkmini/sdram.rs
+++ src/board/hpm6750evkmini/sdram.rs
@@ -7,52 +7,29 @@ pub struct SdramInfo {
 
 macro_rules! init {
     ($p:ident, $delay:ident) => {{
-        let sdram = hpm_hal::femc::Sdram::new_16bit_cs0(
+        let sdram = hpm_hal::femc::Sdram::new_cs0(
             $p.FEMC,
-            // Address pins A0-A11.
-            $p.PC08,
-            $p.PC09,
-            $p.PC04,
-            $p.PC05,
-            $p.PC06,
-            $p.PC07,
-            $p.PC10,
-            $p.PC11,
-            $p.PC12,
-            $p.PC17,
-            $p.PC15,
-            $p.PC21,
-            // Bank address BA0-BA1.
-            $p.PC13,
-            $p.PC14,
-            // Data pins DQ0-DQ15.
-            $p.PD08,
-            $p.PD05,
-            $p.PD00,
-            $p.PD01,
-            $p.PD02,
-            $p.PC27,
-            $p.PC28,
-            $p.PC29,
-            $p.PD04,
-            $p.PD03,
-            $p.PD07,
-            $p.PD06,
-            $p.PD10,
-            $p.PD09,
-            $p.PD13,
-            $p.PD12,
-            // Data mask DM0-DM1.
-            $p.PC30,
-            $p.PC31,
-            // Control: DQS, CLK, CKE, RAS, CAS, WE, CS0.
-            $p.PC16,
-            $p.PC26,
-            $p.PC25,
-            $p.PC18,
-            $p.PC23,
-            $p.PC24,
-            $p.PC19,
+            hpm_hal::femc::SdramPins {
+                address: (
+                    $p.PC08, $p.PC09, $p.PC04, $p.PC05, $p.PC06, $p.PC07, $p.PC10, $p.PC11,
+                    $p.PC12, $p.PC17, $p.PC15, $p.PC21,
+                ),
+                banks: ($p.PC13, $p.PC14),
+                data: (
+                    $p.PD08, $p.PD05, $p.PD00, $p.PD01, $p.PD02, $p.PC27, $p.PC28, $p.PC29,
+                    $p.PD04, $p.PD03, $p.PD07, $p.PD06, $p.PD10, $p.PD09, $p.PD13, $p.PD12,
+                ),
+                masks: ($p.PC30, $p.PC31),
+                control: hpm_hal::femc::ControlPins {
+                    dqs: $p.PC16,
+                    clk: $p.PC26,
+                    cke: $p.PC25,
+                    ras: $p.PC18,
+                    cas: $p.PC23,
+                    we: $p.PC24,
+                    cs: $p.PC19,
+                },
+            },
             hpm_hal::femc::chips::W9812g6jh6,
         );
 
